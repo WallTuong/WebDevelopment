2d1269cd7ff7bcea0a7e5fd2c3149b9111b90e44
Tuong Khanh Nguyen <walltuong@gmail.com>
2023-11-11 02:22:29 +0100

    Dynamic navbar + footer fixed to bottom

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-let age = 21;
-let firstName = "Wallz"
-let student = true;
+/* Navbar - dropdown menu */
+document.addEventListener("click", element => {
+  const isDropdownButton = element.target.matches("[data-dropdown-button]");
+  if (!isDropdownButton && element.target.closest("[data-dropdown]") != null) {
+      return;
+  }
 
-console.log("Hello, ", firstName);
-console.log("You are ", age, "years old");
-console.log("Enrolled: ", !student);
+  let currentDropdown;
+  if (isDropdownButton) {
+      currentDropdown = element.target.closest("[data-dropdown]");
+      currentDropdown.classList.toggle("active");
+  }
 
-document.getElementById("p1").innerHTML = "Hello " + firstName;
-document.getElementById("p2").innerHTML = "You are " + age + " years old";
-document.getElementById("p3").innerHTML = "Enrolled: " + !student;
+  document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+      if (dropdown === currentDropdown) {
+          return;
+      }
+      else {
+          dropdown.classList.remove("active");
+      } 
+  })
+})
 
-document.title = "My website";
-
-Array.prototype.slice(arguments);
+/* Carousel slider */
